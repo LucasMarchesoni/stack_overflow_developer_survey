6f91fbb2e9a9868d435b61171e49e66fe0e76a2c
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-04-30 08:40:05 -0300

    adicionado novos métodos para manipular o banco

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from db.database import DB
 import pandas as pd
 
 df = pd.read_csv("./data/survey_results_public.csv")
-db = DB(df)
+db = DB()
 
 if __name__ == "__main__":
-  db.create_db()
+  db.create_db(df)
--- db/database.py
+++ db/database.py
@@ -1,15 +1,19 @@
 from sqlalchemy import create_engine
+import pandas as pd
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 class DB:
+  def __init__(self):
+    self.engine = create_engine(f'postgresql+psycopg2://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@localhost:5432/{os.environ.get("DB_NAME")}')
   
-  def __init__(self, df):
-    self.df = df
+  def create_db(self, df):
+    df.to_sql('survey', self.engine, index=False, if_exists='fail')
+    
+  def create_connection(self):
+    return self.engine
   
-  def create_db(self):
-    engine = create_engine(f'postgresql+psycopg2://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@localhost:5432/{os.environ.get("DB_NAME")}')
-
-    self.df.to_sql('survey', engine, index=False, if_exists='fail')
+  def query_to_dataframe(self, query):
+    return pd.read_sql_query(query, self.engine)
